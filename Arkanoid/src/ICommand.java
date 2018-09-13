09f97afe06a8cdfb668ef33bf7bc46bb1dd585e3
giovanni <giovanni.gabbolini@gmail.com>
2018-09-13 23:19:28 +0200

    ICommand added to handle generic actions not result of a registered event

---
+++ Arkanoid/src/ICommand.java
@@ -0,0 +1,12 @@
+
+/*
+ * used to model actions to apply to generic items
+ * that are not tick actions or actions derived by the
+ * observation of some other object
+ */
+
+public interface ICommand {
+
+	public void doAction();
+	
+}
